45094271d3a2b8db949c9f5ecb826931e0073b46
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-04-06 17:32:36 -0300

    Adiciona requisito 6 :rocket:

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <div>
+          {planets.map((element) => (<PlanetCard
+            key={ element.name }
+            planetName={ element.name }
+            planetImage={ element.image }
+          />))}
+        </div>
+
       </div>
     );
   }
