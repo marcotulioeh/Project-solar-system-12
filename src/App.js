b5e6ac9b4969cb8ca438bcd0f585c975acb58efa
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-04-06 17:54:25 -0300

    Adiciona requisito 9 :rocket:

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import PlanetCard from './components/PlanetCard';
 import Missions from './components/Missions';
+import MissionCard from './components/MissionCard';
 
 class App extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class App extends React.Component {
         <SolarSystem />
         <Missions />
         <PlanetCard />
+        <MissionCard />
       </>
     );
   }
